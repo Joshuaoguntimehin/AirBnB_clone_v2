6b0525ec45e2264a4809561c757b257fa6bf38bf
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-26 23:44:19 +0100

    alx tasks

--- models/user.py
+++ models/user.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    email = column(string(128), nullable=false)
+    password = column(string(128), nullable=false)
+    first_name = column(string(128), nullable=false)
+    last_name = column(string(128), nullable=false)
