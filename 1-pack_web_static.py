93da4aeb02094a79138af67e2acd0b54630c60a7
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-11 17:55:16 +0100

    alx tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,6 +3,7 @@
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
@@ -24,4 +25,3 @@ def do_pack():
         return archive_name
     else:
         return None
-
