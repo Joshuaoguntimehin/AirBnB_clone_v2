c54cd1cd621d73950263e1ce39b9381f74a060ef
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-27 15:01:43 +0100

    alx tasks

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""import statament"""
+from flask import Flask
+
+"""app definition """
+app = Flask(__name__)
+
+
+"""This defines a route for the root URL (/)"""
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    return "Hello HBNB!"
+
+
+""" This defines a route for the root URL (/). """
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
