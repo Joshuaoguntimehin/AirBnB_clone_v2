bda7e28f98740562b6a6d3f93464c3a98c717b33
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-27 16:12:06 +0100

    alx tasks

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,28 +1,14 @@
-#!/usr/bin/python3
-"""import statement"""
 from flask import Flask
 
-"""app definition"""
 app = Flask(__name__)
 
-"""/: Displays "Hello HBNB!" when accessed."""
-
-
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello():
     return "Hello HBNB!"
-
-
-"""/: Displays "HBNB" when accessed."""
-
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
-
-
-"""which sets the text variable to its default value """
-
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
@@ -30,16 +16,13 @@ def c_text(text):
     text = text.replace('_', ' ')
     return f"C {text}"
 
-
-""" which sets the text variable to its default value "is cool"""
-
-
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    text = text.replace('_' ' ')
-    return f"python {text}"
+    # Replace underscores with spaces
+    text = text.replace('_', ' ')
+    return f"Python {text}"`
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
