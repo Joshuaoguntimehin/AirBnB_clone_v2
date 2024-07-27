76dc90f09ce3e99e3ec83d095f8e5c9da32afdf3
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-27 15:36:32 +0100

    alx tasks

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """import statement"""
-from flask import Flask
+from flask import Flask, escape
 
 """app definition"""
 app = Flask(__name__)
@@ -34,7 +34,7 @@ def c_text(text):
 """ which sets the text variable to its default value "is cool"""
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/', defaults={'text'= 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     text = text.replace('_' ' ')
