66a75a6627b30daa7584da8145ec2b736b5df42c
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-27 15:04:08 +0100

    alx tasks

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,5 +22,12 @@ def hbnb():
     return "HBNB"
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    # Replace underscores with spaces
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
