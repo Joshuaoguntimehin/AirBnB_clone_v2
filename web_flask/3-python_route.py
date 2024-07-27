53edc3f7f1746e30dd0009d39e2e0f34a7bac766
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-27 15:26:47 +0100

    alx tasks

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,44 +1,42 @@
 #!/usr/bin/python3
-"""import statement"""
-from flask import Flask
+"""Import statement"""
+from flask import Flask, escape
 
-"""app definition"""
+"""App definition"""
 app = Flask(__name__)
 
-"""/: Displays "Hello HBNB!" when accessed."""
+"""Displays 'Hello HBNB!' when accessed."""
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
-
-"""/: Displays "HBNB" when accessed."""
+"""Displays 'HBNB' when accessed."""
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
-
-"""which sets the text variable to its default value """
+"""Displays 'C ' followed by the text variable with underscores replaced by spaces."""
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    # Replace underscores with spaces
     text = text.replace('_', ' ')
     return f"C {escape(text)}"
 
+"""Displays 'Python ' followed by the text variable with underscores replaced by spaces.
+Defaults to 'is cool' if no text is provided."""
 
-""" which sets the text variable to its default value "is cool"""
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    text = text.replace('_' ' ')
-    return f"python {escape(text)}"
-
+    text = text.replace('_', ' ')
+    return f"Python {escape(text)}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
