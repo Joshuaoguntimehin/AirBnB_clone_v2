b5a39f4d35d5d16aa83514953a3b1b1b8fa7586b
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-27 14:35:18 +0100

    alx tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""import statement"""
+
 from flask import Flask
 
+"""blocked or used by other applications"""
 app = Flask(__name__)
 
+""" persists, consider providing additional details"""
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    # Configure the app to listen on all interfaces (0.0.0.0) on port 5000
+    """ Configure the app to listen on all interfaces (0.0.0.0) on port 5000"""
     app.run(host='0.0.0.0', port=5000, debug=True)
 
