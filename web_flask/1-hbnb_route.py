ca3b486391efdf911621f66a493e34bcc7203be1
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-27 14:52:11 +0100

    alx tasks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,14 +6,20 @@ from flask import Flask
 app = Flask(__name__)
 
 """ : This defines a route for the root URL (/),"""
+
+
 @app.route('/', strict_slashes=False)
 def Hello_HBNB():
     return "Hello HBNB!"
 
+
 """: This defines a route for the root URL (/)"""
+
+
 @app.route('/', strict_slashes=False)
 def HBMB():
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
