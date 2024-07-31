2d83471782e089d392a12b7c6f9c3cd9c4972f60
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-31 18:18:24 +0100

    alx tasks

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -10,12 +10,16 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+
+
 """/hbnb returns "HBNB"""
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
+
 """/c/<text> replaces underscores with spaces in the text and returns "C """
 
 
@@ -25,6 +29,7 @@ def c_text(text):
     text = text.replace('_', ' ')
     return f"C {text}"
 
+
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
@@ -32,14 +37,19 @@ def python_text(text):
     text = text.replace('_', ' ')
     return f"Python {text}"
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return f"{n} is a number"
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
-@app.route('/number_template/', defaults={'n': 'templates/5-number.html'}, strict_slashes=False)
+@app.route('/number_template/',
+           defaults={'n': 'templates/5-number.html'},
+           strict_slashes=False)
 def number_template(n):
     return f"{n}"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
