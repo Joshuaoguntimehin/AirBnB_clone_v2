e9ab9c927a578476d61116572883084e6eeab1dc
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-31 18:36:01 +0100

    alx tasks

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -55,8 +55,7 @@ def number_template(n):
            defaults={'n': 'templates/6-number_odd_or_even.html'},
            strict_slashes=False):
     odd_or_even = "even" if n % 2 == 0 else "odd"
-    return render_template_string("<html><body><h1>Number: {{ n }} is {{ odd_or_even }}</h1></body></html>", n=n, odd_or_even=odd_or_even)
-
+    return odd_or_even
 
 
 if __name__ == '__main__':
