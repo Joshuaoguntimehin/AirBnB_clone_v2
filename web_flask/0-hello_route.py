5b411d046bc2970e6a8bef2aea9c69a840ca689c
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-08-20 14:00:46 +0000

    new

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 """ persists, consider providing additional details"""
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
